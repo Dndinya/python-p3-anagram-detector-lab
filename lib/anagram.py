3c54a9f8a273fd3dd81d887f460e40a58119514e
Dndinya <derrick.ndinya@gmail.com>
2024-03-07 20:33:11 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+from collections import Counter
+class Anagram:
+    def __init__(self, word):
+        self.word_counts = Counter(word)
+        # self.match = match
+        
+    def match(self, possible_anagrams):
+        match = []
+        for anagram in possible_anagrams:
+            if Counter(anagram) == self.word_counts:
+                match.append(anagram)
+        return match
+                
